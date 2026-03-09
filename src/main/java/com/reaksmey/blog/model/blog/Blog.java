4e66eda0f375cf0aa0926624331d616e61ff55e6
developOnion <smeyonion.nou@gmail.com>
2026-03-09 22:04:49 +0700

    create Blog class and BlogStatus enum

---
+++ src/main/java/com/reaksmey/blog/model/blog/Blog.java
@@ -0,0 +1,48 @@
+package com.reaksmey.blog.model.blog;
+
+import com.reaksmey.blog.model.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "blogs")
+public class Blog extends BaseEntity {
+
+	@NotEmpty
+	@Column(nullable = false)
+	private String title;
+
+	@NotEmpty
+	@Column(nullable = false, unique = true)
+	private String slug;
+
+	@Lob
+	@NotEmpty
+	@Column(nullable = false, columnDefinition = "TEXT")
+	private String content;
+
+	@NotEmpty
+	@Column(length = 500)
+	private String excerpt;
+
+	@NotBlank
+	@Column(nullable = false)
+	private String author;
+
+	private String featuredImageUrl;
+
+	@NotNull
+	@Column(nullable = false)
+	private BlogStatus status;
+}
---
+++ src/main/java/com/reaksmey/blog/model/blog/BlogStatus.java
@@ -0,0 +1,7 @@
+package com.reaksmey.blog.model.blog;
+
+public enum BlogStatus {
+	DRAFT,
+	PUBLISHED,
+	ARCHIVED
+}
